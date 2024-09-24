de8abec7f840b77e322bd3022ad5d39b86c84c67
Quinn <qokabay1@swarthmore.edu>
2024-09-24 09:05:30 -0400

    maybe works now?

---
+++ queries/highlights.scm
@@ -0,0 +1,12 @@
+"fn" @keyword
+"struct" @keyword
+"pub" @keyword
+
+"true" @constant.builtin
+"false" @constant.builtin
+
+["*" "+" "-" "="] @operator
+
+(integer) @number
+["{" "}" "(" ")"] @punctuation.bracket
+["," "." ":"] @punctuation.delimiter
